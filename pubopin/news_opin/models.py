d4a0304218cfca5acc32a7ff2b0da340501c1cc4
liukun <uulink@outlook.com>
2016-05-05 12:43:29 +0800

    maybe here

--- pubopin/news_opin/models.py
+++ pubopin/news_opin/models.py
@@ -8,31 +8,31 @@ class tencent_article(models.Model):
 		ordering=['-put_time']
 		verbose_name="腾讯"
 	news_id = models.CharField(max_length=100)
-	parent_name = models.CharField(max_length=200, default='')#unique for tencent eg:news_news_top
-	news_url = models.CharField(max_length=200,default='')
-	title = models.CharField(max_length=200,default='')
-	abstract = models.CharField(max_length=200,default='')
-	source = models.CharField(max_length=50,default='')
+	parent_name = models.CharField(max_length=200)#unique for tencent eg:news_news_top
+	news_url = models.CharField(max_length=200)
+	title = models.CharField(max_length=200)
+	abstract = models.CharField(max_length=200)
+	source = models.CharField(max_length=50)
 	put_time = models.DateTimeField(u'')
-	content = models.TextField(default='')
-	comments_id = models.CharField(max_length=100,default='')
-	comments_url = models.CharField(max_length=200,default='')
+	content = models.TextField()
+	comments_id = models.CharField(max_length=100)
+	comments_url = models.CharField(max_length=200)
 	comments_number = models.IntegerField(default=0)
 
 class tencent_comment(models.Model):
 	#news=models.ForeignKey(tencent_article,verbose_name="tecent related news")
-	news_id =models.CharField(max_length=100,default='')
-	comments_id = models.CharField(max_length=100)
-	username = models.CharField(max_length=100)
+	news_id =models.CharField(max_length=200)
+	comments_id = models.CharField(max_length=200)
+	username = models.CharField(max_length=200)
 	comment = models.TextField()
 	put_time = models.DateTimeField()
 	sex = models.CharField(max_length=10)
-	reply_id = models.CharField(max_length=100)
+	reply_id = models.CharField(max_length=200)
 	agree_count = models.IntegerField(default=0)
 
 
 class  sina_article(models.Model):
-	news_id=models.CharField(max_length=100)
+	news_id=models.CharField(max_length=200)
 	title = models.CharField(max_length=200)
 	news_url = models.CharField(max_length=200)
 	source=models.CharField(max_length=50)
@@ -43,19 +43,19 @@ class  sina_article(models.Model):
 	comments_number = models.IntegerField(default=0)
 
 class sina_comment(models.Model):
-	news_id =models.CharField(max_length=100)
+	news_id =models.CharField(max_length=200)
 	m_id = models.CharField(unique=True,max_length=200)
 	username = models.CharField(max_length=100)
 	comment = models.TextField()
 	put_time = models.DateTimeField()
 	#sex = models.CharField(max_length=10)
-	parent_id = models.CharField(max_length=100)
+	parent_id = models.CharField(max_length=200)
 	agree_count = models.IntegerField(default=0)
 	against_count=models.IntegerField()
 
 
 class sohu_article(models.Model):
-	news_id=models.CharField(primary_key=True,max_length=100)
+	news_id=models.CharField(primary_key=True,max_length=150)
 	title=models.CharField(max_length=300)
 	put_time=models.DateTimeField()
 	comments_number=models.IntegerField()
@@ -65,9 +65,7 @@ class sohu_article(models.Model):
 class sohu_comment(models.Model):
 	news=models.ForeignKey(sohu_article,verbose_name="sohu related news")
 	#news_id=models.CharField(max_length=100)
-	comments_id=models.CharField(max_length=100)
+	comments_id=models.CharField(max_length=200)
 	author=models.CharField(max_length=50)
 	datetime=models.DateTimeField()
 	comment=models.TextField()
-
-
