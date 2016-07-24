8ae12a7aba1e83565e9939a1a5dc5176414a3529
liukun <uulink@outlook.com>
2016-07-24 22:45:13 +0800

    2016-07-24
    add NLP for comment using snowNLP
    
    front end change

--- pubopin/news_opin/models.py
+++ pubopin/news_opin/models.py
@@ -3,68 +3,69 @@ from django.db import models
 
 # Create your models here.
 class tencent_article(models.Model):
-	"""11 elements"""
-	class Meta:
-		ordering=['-put_time']
-		verbose_name="腾讯"
-	news_id = models.CharField(max_length=100, null=True)
-	parent_name = models.CharField(max_length=200, null=True)#unique for tencent eg:news_news_top
-	news_url = models.CharField(max_length=200, null=True)
-	title = models.CharField(max_length=200, null=True)
-	abstract = models.CharField(max_length=200, null=True)
-	source = models.CharField(max_length=50, null=True)
-	put_time = models.DateTimeField(u'', null=True)
-	content = models.TextField(null=True, default = ' ')
-	comments_id = models.CharField(max_length=100, null=True)
-	comments_url = models.CharField(max_length=200, null=True)
-	comments_number = models.IntegerField(default=0)
+    """11 elements"""
+    class Meta:
+        ordering=['-put_time']
+        verbose_name="腾讯"
+    news_id = models.CharField(max_length=100, null=True)
+    parent_name = models.CharField(max_length=200, null=True)#unique for tencent eg:news_news_top
+    news_url = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200, null=True)
+    abstract = models.CharField(max_length=200, null=True)
+    source = models.CharField(max_length=50, null=True)
+    put_time = models.DateTimeField(u'', null=True)
+    content = models.TextField(null=True, default = ' ')
+    comments_id = models.CharField(max_length=100, null=True)
+    comments_url = models.CharField(max_length=200, null=True)
+    comments_number = models.IntegerField(default=0)
 
 class tencent_comment(models.Model):
-	#news=models.ForeignKey(tencent_article,verbose_name="tecent related news")
-	news_id =models.CharField(max_length=200, null=True)
-	comments_id = models.CharField(max_length=200, null=True)
-	username = models.CharField(max_length=200, null=True)
-	comment = models.TextField(null=True, default = ' ')
-	put_time = models.DateTimeField(null=True)
-	sex = models.CharField(max_length=10,null=True)
-	reply_id = models.CharField(max_length=200, null=True)
-	agree_count = models.IntegerField(default=0,null=True)
+    #news=models.ForeignKey(tencent_article,verbose_name="tecent related news")
+    news_id =models.CharField(max_length=200, null=True)
+    comments_id = models.CharField(max_length=200, null=True)
+    username = models.CharField(max_length=200, null=True)
+    comment = models.TextField(null=True, default = ' ')
+    put_time = models.DateTimeField(null=True)
+    sex = models.CharField(max_length=10,null=True)
+    reply_id = models.CharField(max_length=200, null=True)
+    agree_count = models.IntegerField(default=0,null=True)
 
 
 class  sina_article(models.Model):
-	news_id=models.CharField(max_length=200, null=True)
-	title = models.CharField(max_length=200, null=True)
-	news_url = models.CharField(max_length=200, null=True)
-	source=models.CharField(max_length=50, null=True)
-	category=models.CharField(max_length=50, null=True)
-	put_time=models.DateTimeField(null=True)
-	content=models.TextField(null=True, default = ' ')
-	comments_url=models.CharField(max_length=250, null=True)
-	comments_number = models.IntegerField(default=0, null=True)
+    news_id=models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200, null=True)
+    news_url = models.CharField(max_length=200, null=True)
+    source=models.CharField(max_length=50, null=True)
+    category=models.CharField(max_length=50, null=True)
+    put_time=models.DateTimeField(null=True)
+    content=models.TextField(null=True, default = ' ')
+    comments_url=models.CharField(max_length=250, null=True)
+    comments_number = models.IntegerField(default=0, null=True)
 
 class sina_comment(models.Model):
-	news_id =models.CharField(max_length=200, null=True)
-	m_id = models.CharField(unique=True,max_length=200)
-	username = models.CharField(max_length=100, null=True)
-	comment = models.TextField(null=True, default = ' ')
-	put_time = models.DateTimeField(null=True)
-	#sex = models.CharField(max_length=10)
-	parent_id = models.CharField(max_length=200, null=True)
-	agree_count = models.IntegerField(default=0, null=True)
-	against_count=models.IntegerField(null=True)
-
+    news_id =models.CharField(max_length=200, null=True)
+    m_id = models.CharField(unique=True,max_length=200)
+    username = models.CharField(max_length=100, null=True)
+    comment = models.TextField(null=True, default = ' ')
+    put_time = models.DateTimeField(null=True)
+    #sex = models.CharField(max_length=10)
+    parent_id = models.CharField(max_length=200, null=True)
+    agree_count = models.IntegerField(default=0, null=True)
+    against_count=models.IntegerField(null=True)
+    sence_color = models.CharField(max_length=20, null=True)
 
 class sohu_article(models.Model):
-	news_id=models.CharField(primary_key=True,max_length=150)
-	title=models.CharField(max_length=300, null=True)
-	put_time=models.DateTimeField(null=True)
-	comments_number=models.IntegerField(null=True)
-	content=models.TextField(null=True, default = ' ')
-	news_url=models.CharField(max_length=200,null=True)
+    news_id=models.CharField(primary_key=True,max_length=150)
+    title=models.CharField(max_length=300, null=True)
+    put_time=models.DateTimeField(null=True)
+    comments_number=models.IntegerField(null=True)
+    content=models.TextField(null=True, default = ' ')
+    news_url=models.CharField(max_length=200,null=True)
 
 class sohu_comment(models.Model):
-	news=models.ForeignKey(sohu_article,verbose_name="sohu related news")
-	comments_id=models.CharField(max_length=200, null=True)
-	author=models.CharField(max_length=50, null=True)
-	datetime=models.DateTimeField(null=True)
-	comment=models.TextField(null=True, default = ' ')
+    news=models.ForeignKey(sohu_article,verbose_name="sohu related news")
+    comments_id=models.CharField(max_length=200, null=True)
+    author=models.CharField(max_length=50, null=True)
+    datetime=models.DateTimeField(null=True)
+    comment=models.TextField(null=True, default = ' ')
+    sence_color = models.CharField(max_length=20, null=True)
--- pubopin/news_opin/views.py
+++ pubopin/news_opin/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import tencent_article,tencent_comment,sina_article,sina_comment,sohu_article,sohu_comment
 from django.http import HttpResponse,Http404
+from snownlp import SnowNLP as nlp
 
 def show(request):
-    delta=datetime.timedelta(days=2)#展示日期区间
+    delta=datetime.timedelta(days=100)#展示日期区间
     hot_sina_news=sina_article.objects.filter(put_time__gte=datetime.date.today()-delta).exclude(title__isnull=True).exclude(title__exact='').order_by('-comments_number')[:20]
     hot_tencent_news=tencent_article.objects.filter(put_time__gte=datetime.date.today()-delta).exclude(title__isnull=True).exclude(title__exact='').order_by('-comments_number')[:20]
     hot_sohu_news=sohu_article.objects.filter(put_time__gte=datetime.date.today()-delta).exclude(title__isnull=True).exclude(title__exact='').order_by('-comments_number')[:20]
@@ -24,19 +25,63 @@ def show(request):
 def sina_cmt(request,comment_id):
     comment_id='comos-'+comment_id
     lastern_comments=sina_comment.objects.filter(news_id=comment_id).order_by('-put_time')[:50]
+    attitude_tendency = []
+    for com in lastern_comments:
+        s = nlp(com.comment)
+        senti_number = s.sentiments
+        attitude_tendency.append(senti_number)
+        rgb_list = ["#565D8E", "#499AA1","#FFFFFF", "#F9E063", "#BBDC37"]
+        rgb_level = 2
+        if senti_number  > 0.7 and senti_number <0.8:
+            rgb_level = 3
+        elif senti_number >=0.8:
+            rgb_level = 4
+        elif senti_number < 0.35 and senti_number > 0.25:
+            rgb_level = 1
+        elif senti_number <= 0.25:
+            rgb_level = 0
+        #senti_rgb = str(hex(int( senti_number * 0xFFFFFF))).replace("0x", "#").upper()
+        senti_rgb = rgb_list[rgb_level]
+        com.sense_color = senti_rgb
+    tendency_number = sum(attitude_tendency)/len(attitude_tendency)
+    attitude_list = ["很消极", "消极", "中立", "积极", "很积极"]
+    attitude = attitude_list[ int(round(tendency_number*10/2)) -1 ]
     #return HttpResponse(comment_id)#str(sina_comment.objects.filter(news_id=comment_id).values("username")))    
     return render(request, 'news_opin/sina_comment_list.html',
            {
              'comments_list':lastern_comments,
+             'attitude':attitude + "  积极指数:"+ str(tendency_number),
            }  
           )
 
 def sohu_cmt(request,comment_id):
     #return HttpResponse(str(sohu_comment.objects.filter(news_id=comment_id).values("author")))#留作DEBUG用
     lastern_comments=sohu_comment.objects.filter(news_id=comment_id)[:50]
+    attitude_tendency = []
+    for com in lastern_comments:
+        s = nlp(com.comment)
+        senti_number = s.sentiments
+        attitude_tendency.append(senti_number)
+        rgb_list = ["#565D8E", "#499AA1","#FFFFFF", "#F9E063", "#BBDC37"]
+        rgb_level = 2
+        if senti_number  > 0.7 and senti_number <0.8:
+            rgb_level = 3
+        elif senti_number >=0.8:
+            rgb_level = 4
+        elif senti_number < 0.35 and senti_number > 0.25:
+            rgb_level = 1
+        elif senti_number <= 0.25:
+            rgb_level = 0
+        #senti_rgb = str(hex(int( senti_number * 0xFFFFFF))).replace("0x", "#").upper()
+        senti_rgb = rgb_list[rgb_level]
+        com.sense_color = senti_rgb
+    tendency_number = sum(attitude_tendency)/len(attitude_tendency)
+    attitude_list = ["很消极", "消极", "中立", "积极", "很积极"]
+    attitude = attitude_list[ int(round(tendency_number*10/2)) -1 ]
     return render(request, 'news_opin/sohu_comment_list.html',
            {
              'sohu_comments_list':lastern_comments,
+             'attitude':attitude + "  积极指数:"+ str(tendency_number),
            }  
           )
 
